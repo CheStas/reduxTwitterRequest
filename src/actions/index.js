c4b44952f18f48973ef9e081faf97cca769753e5
Stas <chstas2@gmail.com>
2017-11-20 08:01:53 +0200

    add wrong request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 export const FETCH_TWITTERS = 'FETCH_TWITTERS';
+const bearer_token = 'AAAAAAAAAAAAAAAAAAAAAAq13AAAAAAACiMObO2Ouy42H%2B00yg9F3rZVlmU%3DwFyg8xRTJbEmOp1fuS27UFWEFUQGf3oa0DKunPrpsUdNcoKmqu';
 
 export function fetchTwitters(person) {
-    const url = `https://api.twitter.com/1.1/search/tweets.json?${person}`;
-    const request = axios.get(url, { crossdomain: true });
+    const url = `https://api.twitter.com/1.1/search/tweets.json?q=${person}`;
+    const request = axios.get(url, {crossdomain: true, headers: { "Authorization": "Bearer " + bearer_token , "Access-Control-Allow-Origin": "*"}});
 
     return {
         type: FETCH_TWITTERS,
--- src/reducers/reducer_twitters.js
+++ src/reducers/reducer_twitters.js
@@ -3,6 +3,7 @@ import { FETCH_TWITTERS } from '../actions/index'
 export default function (state = [], action) {
     switch (action.type) {
         case FETCH_TWITTERS:
+            console.log(action.payload);
             return action.payload;
     }
     return state;
